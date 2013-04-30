83d2824fb70c9ce0422077ac9bd83def70c6e30d
Lee Walker <leethomaswalker91@googlemail.com>
2013-04-30 01:09:46 +0100

    Preliminary Design

---
+++ index.php
@@ -0,0 +1,26 @@
+<! DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			Foamy Panda Web Creative
+		</title>
+	</head>
+	<body>
+		<div class="menu">
+			<ul>
+			<li>About Foamy Panda Creations</li>
+			<li>The Team</li>
+			<li>Our Affiliates</li>
+			<li>Contact Us</li>
+			</ul>
+		</div>
+		<div class="welcome">
+			<p>
+				Hello and welcome to Foamy Panda Creations!
+				We are the first port of call for your web needs. 
+				Whatever the need of your website, we are here to make it a reality.
+				Please folow the links on the left hand side of this page to find out more about us and our organisation.
+			</p>
+		</div>
+	</body>
+</html>
